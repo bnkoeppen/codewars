b99ee3f644d1a4fc9732069e50321b975e6d0c72
bnkoeppen <bnkoeppen@gmail.com>
2026-04-21 16:48:34 -0400

    69cda5b85599f307742ce70a solution

--- js/kyu7/69cda5b85599f307742ce70a/solution.js
+++ js/kyu7/69cda5b85599f307742ce70a/solution.js
@@ -1,4 +1,26 @@
 function countLonelyLetters(text) {
-  // your code here
-  return 0;
+  const ascii_a = "a".charCodeAt(0);
+  const re = /[^a-zA-Z]/g;
+
+  const filtered_text = text.replaceAll(re, "").toLowerCase();
+  let char_counts = new Array(26).fill(0);
+  for (let char of filtered_text) {
+    char_counts[char.charCodeAt(0) - ascii_a]++;
+  }
+
+  let acc = 0;
+  for (let idx = 0; idx < char_counts.length; idx++) {
+    let neighbors;
+    if (idx === 0) {
+      neighbors = [idx + 1];
+    } else if (idx === char_counts.length - 1) {
+      neighbors = [idx - 1];
+    } else {
+      neighbors = [idx - 1, idx + 1];
+    }
+    acc +=
+      neighbors.every((v) => char_counts[v] === 0) && char_counts[idx] === 1;
+  }
+
+  return acc;
 }
