697bca7baf30d7dca7d098b1b11b2bd485b094b3
Benjamin Koeppen <bnkoeppen@gmail.com>
2026-04-25 02:06:36 -0400

    58f5c63f1e26ecda7e000029 oneliner

--- js/kyu6/58f5c63f1e26ecda7e000029/solution.js
+++ js/kyu6/58f5c63f1e26ecda7e000029/solution.js
@@ -1,10 +1,7 @@
 function wave(str) {
-  if (str.trim() === "") return [];
-  const letterIdxs = [...str].reduce(
-    (acc, char, i) => (char === " " ? acc : acc.concat(i)),
-    [],
-  );
-  return letterIdxs.map(
-    (i) => str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1),
-  );
+  return str.trim() === ""
+    ? []
+    : [...str]
+        .reduce((acc, char, i) => (char === " " ? acc : acc.concat(i)), [])
+        .map((i) => str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1));
 }
