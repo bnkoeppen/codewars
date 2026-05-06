29cb0032a817021d47282291fd13c52197f28eaa
bnkoeppen <bnkoeppen@gmail.com>
2026-05-05 21:19:27 -0400

    fix merge conflict

--- js/kyu5/52597aa56021e91c93000cb0/solution.js
+++ js/kyu5/52597aa56021e91c93000cb0/solution.js
@@ -1,8 +1,2 @@
 const moveZeros = (arr) =>
-<<<<<<< HEAD
-  arr
-    .filter((v) => v !== 0)
-    .concat(Array(arr.reduce((acc, v) => (acc += v === 0), 0)).fill(0));
-=======
   arr.filter((v) => v !== 0).concat(arr.filter((v) => v === 0));
->>>>>>> 8cfd2dd (52597aa56021e91c93000cb0 solution)
